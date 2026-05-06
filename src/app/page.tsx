02c6adf42509d000f6f9a49418ab0e2f05415952
muhammed shareef cv <muhammedshareefcv@muhammeds-MacBook-Air.local>
2026-05-06 15:27:52 +0530

    Remove New Arrivals section from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import HeroBanner from "@/components/homepage/Header";
 import CategoryCarousel from "@/components/homepage/CategoryCarousel";
-import NewArrivals from "@/components/homepage/NewArrivals";
 import TopSelling from "@/components/homepage/TopSelling";
 
-export const revalidate = 60;
 
 export default function Home() {
   return (
@@ -11,10 +9,6 @@ export default function Home() {
       <HeroBanner />
       <main className="my-[50px] sm:my-[72px]">
         <CategoryCarousel />
-        <NewArrivals />
-        <div className="max-w-frame mx-auto px-4 xl:px-0">
-          <hr className="h-[1px] border-t-brand/10 my-10 sm:my-16" />
-        </div>
         <TopSelling />
       </main>
     </>
